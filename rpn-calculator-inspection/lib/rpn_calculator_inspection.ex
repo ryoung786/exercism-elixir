bbe10e899eb399a99cd6b099f06eed58336bf41d
Ryan Young <ryan.young@thescore.com>
2022-04-19 23:54:05 -0400

    rpn calculator inpection

---
+++ rpn-calculator-inspection/lib/rpn_calculator_inspection.ex
@@ -0,0 +1,34 @@
+defmodule RPNCalculatorInspection do
+  def start_reliability_check(calculator, input) do
+    {:ok, pid} = Task.start_link(fn -> calculator.(input) end)
+
+    %{input: input, pid: pid}
+  end
+
+  def await_reliability_check_result(%{pid: pid, input: input}, results) do
+    result =
+      receive do
+        {:EXIT, ^pid, :normal} -> :ok
+        {:EXIT, ^pid, _} -> :error
+      after
+        100 -> :timeout
+      end
+
+    Map.put(results, input, result)
+  end
+
+  def reliability_check(calculator, inputs) do
+    orig_flag = Process.flag(:trap_exit, true)
+
+    inputs
+    |> Enum.map(&start_reliability_check(calculator, &1))
+    |> Enum.reduce(%{}, &await_reliability_check_result/2)
+    |> tap(fn _ -> Process.flag(:trap_exit, orig_flag) end)
+  end
+
+  def correctness_check(calculator, inputs) do
+    inputs
+    |> Enum.map(fn input -> Task.async(fn -> calculator.(input) end) end)
+    |> Task.await_many(100)
+  end
+end
