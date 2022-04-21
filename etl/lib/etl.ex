d500d5256480b6a593ac9d06e3c8de63e4d029d5
Ryan Young <ryan.young@thescore.com>
2022-04-20 22:03:41 -0400

    etl

---
+++ etl/lib/etl.ex
@@ -0,0 +1,16 @@
+defmodule ETL do
+  @doc """
+  Transforms an old Scrabble score system to a new one.
+
+  ## Examples
+
+    iex> ETL.transform(%{1 => ["A", "E"], 2 => ["D", "G"]})
+    %{"a" => 1, "d" => 2, "e" => 1, "g" => 2}
+  """
+  @spec transform(map) :: map
+  def transform(input) do
+    for {val, letters} <- input, letter <- letters, reduce: %{} do
+      acc -> Map.put_new(acc, String.downcase(letter), val)
+    end
+  end
+end
