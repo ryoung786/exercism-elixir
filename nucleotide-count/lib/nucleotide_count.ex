7cd9f8a42fb0ffea72ac2e398398997cabc6069f
Ryan Young <ryan.young@thescore.com>
2022-04-26 18:42:01 -0400

    nucleotide count

---
+++ nucleotide-count/lib/nucleotide_count.ex
@@ -0,0 +1,32 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a DNA strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count('AATAA', ?A)
+  4
+
+  iex> NucleotideCount.count('AATAA', ?T)
+  1
+  """
+  @spec count(charlist(), char()) :: non_neg_integer()
+  def count(strand, nucleotide) when nucleotide in @nucleotides do
+    Map.get(histogram(strand), nucleotide)
+  end
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram(charlist()) :: map()
+  def histogram(strand) do
+    Map.merge(%{?A => 0, ?T => 0, ?C => 0, ?G => 0}, Enum.frequencies(strand))
+  end
+end
