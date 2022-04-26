f19e28c7a2cdd0a036b565aeb65c670fe3b1cd55
Ryan Young <ryan.young@thescore.com>
2022-04-26 12:34:21 -0400

    use Bitwise

--- allergies/lib/allergies.ex
+++ allergies/lib/allergies.ex
@@ -1,4 +1,6 @@
 defmodule Allergies do
+  use Bitwise
+
   @doc """
   List the allergies for which the corresponding flag bit is true.
   """
@@ -12,12 +14,12 @@ defmodule Allergies do
   Returns whether the corresponding flag bit in 'flags' is set for the item.
   """
   @spec allergic_to?(non_neg_integer, String.t()) :: boolean
-  def allergic_to?(flags, "eggs"), do: Bitwise.&&&(flags, 1) == 1
-  def allergic_to?(flags, "peanuts"), do: Bitwise.&&&(flags, 2) == 2
-  def allergic_to?(flags, "shellfish"), do: Bitwise.&&&(flags, 4) == 4
-  def allergic_to?(flags, "strawberries"), do: Bitwise.&&&(flags, 8) == 8
-  def allergic_to?(flags, "tomatoes"), do: Bitwise.&&&(flags, 16) == 16
-  def allergic_to?(flags, "chocolate"), do: Bitwise.&&&(flags, 32) == 32
-  def allergic_to?(flags, "pollen"), do: Bitwise.&&&(flags, 64) == 64
-  def allergic_to?(flags, "cats"), do: Bitwise.&&&(flags, 128) == 128
+  def allergic_to?(flags, "eggs"), do: (flags &&& 1) == 1
+  def allergic_to?(flags, "peanuts"), do: (flags &&& 2) == 2
+  def allergic_to?(flags, "shellfish"), do: (flags &&& 4) == 4
+  def allergic_to?(flags, "strawberries"), do: (flags &&& 8) == 8
+  def allergic_to?(flags, "tomatoes"), do: (flags &&& 16) == 16
+  def allergic_to?(flags, "chocolate"), do: (flags &&& 32) == 32
+  def allergic_to?(flags, "pollen"), do: (flags &&& 64) == 64
+  def allergic_to?(flags, "cats"), do: (flags &&& 128) == 128
 end
