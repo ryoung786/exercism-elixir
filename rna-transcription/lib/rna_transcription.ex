33ab3706fa6257b750aa42116c62479fa970d8d1
Ryan Young <ryan.young@thescore.com>
2022-06-30 18:50:04 -0400

    rna-transcription

---
+++ rna-transcription/lib/rna_transcription.ex
@@ -0,0 +1,19 @@
+defmodule RnaTranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RnaTranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    Enum.map(dna, fn
+      ?A -> ?U
+      ?C -> ?G
+      ?T -> ?A
+      ?G -> ?C
+    end)
+  end
+end
