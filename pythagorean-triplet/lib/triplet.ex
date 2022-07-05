7e17d2c502971b759cb8cc1c14aed97a4b65b2d6
Ryan Young <ryan.young@thescore.com>
2022-07-05 11:51:49 -0400

    pythagorean triplet

---
+++ pythagorean-triplet/lib/triplet.ex
@@ -0,0 +1,35 @@
+defmodule Triplet do
+  @doc """
+  Calculates sum of a given triplet of integers.
+  """
+  @spec sum([non_neg_integer]) :: non_neg_integer
+  def sum(triplet) do
+    Enum.sum(triplet)
+  end
+
+  @doc """
+  Calculates product of a given triplet of integers.
+  """
+  @spec product([non_neg_integer]) :: non_neg_integer
+  def product(triplet) do
+    Enum.product(triplet)
+  end
+
+  @doc """
+  Determines if a given triplet is pythagorean. That is, do the squares of a and b add up to the square of c?
+  """
+  @spec pythagorean?([non_neg_integer]) :: boolean
+  def pythagorean?([a, b, c]) do
+    a * a + b * b == c * c
+  end
+
+  @doc """
+  Generates a list of pythagorean triplets whose values add up to a given sum.
+  """
+  @spec generate(non_neg_integer) :: [list(non_neg_integer)]
+  def generate(n) do
+    for a <- 1..n, b <- (a + 1)..n, pythagorean?([a, b, n - a - b]) do
+      [a, b, n - a - b]
+    end
+  end
+end
