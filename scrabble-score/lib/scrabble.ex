ea7f726807d4cf76d06158b1eb9d967ecf4b05d5
Ryan Young <ryan.young@thescore.com>
2022-07-05 13:23:24 -0400

    scrabble remove frequencies, not needed

--- scrabble-score/lib/scrabble.ex
+++ scrabble-score/lib/scrabble.ex
@@ -3,28 +3,19 @@ defmodule Scrabble do
   Calculate the scrabble score for the word.
   """
 
-  @letters1 ~w(A E I O U L N R S T)
-  @letters2 ~w(D G)
-  @letters3 ~w(B C M P)
-  @letters4 ~w(F H V W Y)
-  @letters5 ~w(K)
-  @letters8 ~w(J X)
-  @letters10 ~w(Q Z)
-
   @spec score(String.t()) :: non_neg_integer
   def score(word) do
     word
     |> String.upcase()
     |> String.graphemes()
-    |> Enum.frequencies()
     |> Enum.map(fn
-      {ch, n} when ch in @letters1 -> n
-      {ch, n} when ch in @letters2 -> 2 * n
-      {ch, n} when ch in @letters3 -> 3 * n
-      {ch, n} when ch in @letters4 -> 4 * n
-      {ch, n} when ch in @letters5 -> 5 * n
-      {ch, n} when ch in @letters8 -> 8 * n
-      {ch, n} when ch in @letters10 -> 10 * n
+      ch when ch in ~w(A E I O U L N R S T) -> 1
+      ch when ch in ~w(D G) -> 2
+      ch when ch in ~w(B C M P) -> 3
+      ch when ch in ~w(F H V W Y) -> 4
+      ch when ch in ~w(K) -> 5
+      ch when ch in ~w(J X) -> 8
+      ch when ch in ~w(Q Z) -> 10
       _ -> 0
     end)
     |> Enum.sum()
