875646aacf3580cb0e45149e78e84283603ae70e
Ryan Young <ryan.young@thescore.com>
2022-07-10 16:17:26 -0400

    grains

---
+++ grains/lib/grains.ex
@@ -0,0 +1,20 @@
+defmodule Grains do
+  @doc """
+  Calculate two to the power of the input minus one.
+  """
+  @spec square(pos_integer()) :: {:ok, pos_integer()} | {:error, String.t()}
+  def square(number) when number in 1..64, do: {:ok, Integer.pow(2, number - 1)}
+  def square(_), do: {:error, "The requested square must be between 1 and 64 (inclusive)"}
+
+  @doc """
+  Adds square of each number from 1 to 64.
+  """
+  @spec total :: {:ok, pos_integer()}
+  def total do
+    {:ok,
+     Enum.reduce(1..64, 0, fn n, acc ->
+       {:ok, sq} = square(n)
+       acc + sq
+     end)}
+  end
+end
