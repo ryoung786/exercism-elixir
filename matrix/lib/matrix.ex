04538370aa14b470240b28b35c8bac873a8dcb7f
Ryan Young <ryan.young@thescore.com>
2022-07-11 11:10:44 -0400

    matrix

---
+++ matrix/lib/matrix.ex
@@ -0,0 +1,69 @@
+defmodule Matrix do
+  defstruct matrix: nil
+
+  @doc """
+  Convert an `input` string, with rows separated by newlines and values
+  separated by single spaces, into a `Matrix` struct.
+  """
+  @spec from_string(input :: String.t()) :: %Matrix{}
+  def from_string(input) do
+    m =
+      input
+      |> String.split("\n")
+      |> Enum.map(&String.split(&1, " "))
+
+    %Matrix{matrix: m}
+  end
+
+  @doc """
+  Write the `matrix` out as a string, with rows separated by newlines and
+  values separated by single spaces.
+  """
+  @spec to_string(matrix :: %Matrix{}) :: String.t()
+  def to_string(%Matrix{matrix: matrix}) do
+    matrix
+    |> Enum.map(&Enum.join(&1, " "))
+    |> Enum.join("\n")
+  end
+
+  @doc """
+  Given a `matrix`, return its rows as a list of lists of integers.
+  """
+  @spec rows(matrix :: %Matrix{}) :: list(list(integer))
+  def rows(%Matrix{matrix: matrix}) do
+    matrix
+    |> Enum.map(fn row -> Enum.map(row, &String.to_integer/1) end)
+  end
+
+  @doc """
+  Given a `matrix` and `index`, return the row at `index`.
+  """
+  @spec row(matrix :: %Matrix{}, index :: integer) :: list(integer)
+  def row(matrix, index) do
+    rows(matrix)
+    |> Enum.at(index - 1)
+  end
+
+  @doc """
+  Given a `matrix`, return its columns as a list of lists of integers.
+  """
+  @spec columns(matrix :: %Matrix{}) :: list(list(integer))
+  def columns(%Matrix{matrix: rows}) do
+    height = length(rows)
+    width = length(hd(rows))
+
+    for col <- 0..(width - 1), row <- 0..(height - 1) do
+      rows |> Enum.at(row) |> Enum.at(col) |> String.to_integer()
+    end
+    |> Enum.chunk_every(height)
+  end
+
+  @doc """
+  Given a `matrix` and `index`, return the column at `index`.
+  """
+  @spec column(matrix :: %Matrix{}, index :: integer) :: list(integer)
+  def column(matrix, index) do
+    columns(matrix)
+    |> Enum.at(index - 1)
+  end
+end
